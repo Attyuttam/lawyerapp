151e102ccf783afec371d82f942693863f17fef5
Attyuttam Saha <attyuttam@gmail.com>
2020-04-22 09:29:53 +0530

    LP-1: initial setup

---
+++ src/main/java/com/lawyerinc/lawyerapp/LawyerappApplication.java
@@ -0,0 +1,13 @@
+package com.lawyerinc.lawyerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LawyerappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LawyerappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lawyerinc/lawyerapp/LawyerappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lawyerinc.lawyerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LawyerappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
